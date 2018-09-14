cc301a63c502469d0e76e7463b74c241edb13810
Wayan Jimmy <jimmyeatcrab@gmail.com>
2018-09-14 09:55:13 +0800

    update readme

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
 # React Shopping Cart Workshop
+
 Workshop membuat shopping cart menggunakan React, [Create React App](https://github.com/facebookincubator/create-react-app).
 
+## Yang akan dipelajari
+
+1. Functional dan Class Component
+2. State dan Lifecycle Component
+3. Event handling
+4. Conditional render
+5. Ajax Request dengan `fetch()`
+6. List render
+
 ## Cara menjalankan project
+
 1. Menuju ke root project via terminal.
-1. Jalankan perintah ```yarn``` atau ```yarn install``` atau ```npm install``` untuk menginstall dependencies yang diperlukan.
-1. Untuk melihat di browser gunakan perintah ```yarn start``` atau ```npm run start```
+1. Jalankan perintah `yarn` atau `yarn install` atau `npm install` untuk menginstall dependencies yang diperlukan.
+1. Untuk melihat di browser gunakan perintah `yarn start` atau `npm run start`
 
 ## Cara berkontribusi
-erdapat tiga jalan bagi Anda jika berkontribusi melalui Github:
+
+Terdapat tiga jalan bagi Anda jika berkontribusi melalui Github:
+
 1. Memberikan star ⭐️ pada repository ini.
 1. Mengirimkan [issue](https://github.com/wayanjimmy/react-shopping-cart-workshop).
-2. Mengirimkan pull request. <br />
-   Jika Anda mengirimkan pull request, pastikan Anda tidak melakukan update pada branch master 
+1. Mengirimkan pull request. <br />
+   Jika Anda mengirimkan pull request, pastikan Anda tidak melakukan update pada branch master
    agar memudahkan maintainer project dan tidak mengganggu jalannya repository yang sedang dikerjakan.
-   Nb: *Kebijakan ini kembali kepada maintainer project*
+   Nb: _Kebijakan ini kembali kepada maintainer project_
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -3,57 +3,54 @@ import PropTypes from 'prop-types';
 
 import Price from './Price';
 
-class ShoppingCart extends React.Component {
-  static propTypes = {
-    cartItems: PropTypes.array.isRequired,
-    handleRemoveItemFromCart: PropTypes.func.isRequired
-  };
-
-  static defaultProps = {
-    cartItems: []
-  };
-
-  render() {
-    const { cartItems, handleRemoveItemFromCart } = this.props;
-    const total = cartItems.reduce((carrier, cartItem) => {
-      return carrier + cartItem.quantity * cartItem.product.price;
-    }, 0);
-    return (
-      <div className="column">
-        <h3 className="title is-4">Shopping Cart</h3>
-        <table className="table is-fullwidth">
-          <thead>
-            <tr>
-              <th>Item Name</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th />
+const ShoppingCart = ({ cartItems, handleRemoveItemFromCart }) => {
+  const total = cartItems.reduce((carrier, cartItem) => {
+    return carrier + cartItem.quantity * cartItem.product.price;
+  }, 0);
+  return (
+    <div className="column">
+      <h3 className="title is-4">Shopping Cart</h3>
+      <table className="table is-fullwidth">
+        <thead>
+          <tr>
+            <th>Item Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map((item, index) => (
+            <tr key={index}>
+              <td>{item.product.name}</td>
+              <td>{item.product.price}</td>
+              <td>{item.quantity}</td>
+              <td>
+                <button
+                  className="button is-danger is-small"
+                  onClick={() => handleRemoveItemFromCart(item)}
+                >
+                  -
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {cartItems.map((item, index) => (
-              <tr key={index}>
-                <td>{item.product.name}</td>
-                <td>{item.product.price}</td>
-                <td>{item.quantity}</td>
-                <td>
-                  <button
-                    className="button is-danger is-small"
-                    onClick={() => handleRemoveItemFromCart(item)}
-                  >
-                    -
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <h3>
-          Total : <Price amount={total} />
-        </h3>
-      </div>
-    );
-  }
-}
+          ))}
+        </tbody>
+      </table>
+      <h3>
+        Total : <Price amount={total} />
+      </h3>
+    </div>
+  );
+};
+
+ShoppingCart.propTypes = {
+  cartItems: PropTypes.array.isRequired,
+  handleRemoveItemFromCart: PropTypes.func.isRequired
+};
+
+ShoppingCart.defaultProps = {
+  cartItems: []
+};
 
 export default ShoppingCart;
